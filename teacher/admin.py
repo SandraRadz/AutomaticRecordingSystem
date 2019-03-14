baa7f0312da5c9e71e411ba1201e226f73af9b4a
SandraRadz <oleksandraradzievska@gmail.com>
2019-03-14 13:10:49 +0200

    teacher's models, migrations, admin

--- teacher/admin.py
+++ teacher/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from teacher.models import *
 
 # Register your models here.
+admin.site.register(Faculty)
+admin.site.register(Department)
+admin.site.register(Teacher)
+
--- teacher/models.py
+++ teacher/models.py
@@ -1,9 +1,36 @@
 from django.db import models
 
-# Create your models here.
-class Teacher(models.Model):
-    lastName = models.CharField(max_length=35)
-    firstName = models.CharField(max_length=35)
-    secondName = models.CharField(max_length=35)
 
-    
+# Create your models here.
+class Faculty(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    dean = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, to_field='name')
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=35, unique=True)
+    office_365_email = models.EmailField()
+    additional_email = models.EmailField(null=True, blank=True)
+    degree = models.CharField(max_length=50, null=True, blank=True)
+    # naukovuy stypin
+    # posada
+    office = models.CharField(max_length=250, null=True, blank=True)
+    phone = models.CharField(max_length=250, null=True, blank=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, to_field='name')
+
+    def __str__(self):
+        return self.name
+
+
+
