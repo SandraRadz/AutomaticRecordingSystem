4f9c12d74dccd125f6a955ab65c831be1013bc41
SandraRadz <oleksandraradzievska@gmail.com>
2019-04-02 10:13:04 +0300

    it seems nothing have broken

--- authorization/urls.py
+++ authorization/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.urls import include
 from authorization import views
 
+app_name = 'authorization'
 urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
