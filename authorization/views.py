f1db47afbcba2949c61a5143cf3351dc3654108d
unknown <v.chumak@ukma.edu.ua>
2019-04-05 02:41:35 +0300

    added users' diff

--- authorization/views.py
+++ authorization/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.urls import reverse
 from authorization.authhelper import get_signin_url, get_token_from_code
 from authorization.outlookservice import get_me
+from student.models import Student
+from teacher.models import Teacher, Methodist
 
 
 def home(request):
@@ -15,7 +17,9 @@ def home(request):
 
 def exit(request):
     request.session.pop('mail')
+    request.session.pop('fullName')
     return HttpResponseRedirect('../authorization/')
+
 
 def gettoken(request):
     auth_code = request.GET['code']
@@ -23,11 +27,21 @@ def gettoken(request):
     token = get_token_from_code(auth_code, redirect_uri)
     access_token = token['access_token']
     info = get_me(access_token)
-    request.session['mail'] = info['mail']
     username = info['mail'].split('@')[0]
     if not User.objects.filter(username=username).exists():
         user = User.objects.create_user(username, info['mail'])
         user.first_name = info['displayName'].split(' ')[1]
         user.last_name = info['displayName'].split(' ')[0]
         user.save()
+    user = User.objects.get(username=username)
+    request.session['mail'] = info['mail']
+    request.session['fullName'] = info['displayName']
+    role = None
+    if Student.objects.filter(student_id=user.id).exists():
+        role = 'student'
+    elif Teacher.objects.filter(teacher_id=user.id).exists():
+        role = 'teacher'
+    elif Methodist.objects.filter(methodist_id=user.id).exists():
+        role = 'methodist'
+    request.session['role'] = role
     return HttpResponseRedirect('../../theme/')
