6da7c3c609f92570fb22a73b716e78a363ba0578
unknown <v.chumak@ukma.edu.ua>
2019-04-05 05:13:51 +0300

    search fixes

--- student/views.py
+++ student/views.py
@@ -2,8 +2,28 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import DetailView
+
+from student.models import Student
+
+
 def index(request):
     if 'mail' not in request.session:
         return HttpResponseRedirect('../authorization/')
     return HttpResponse('<h1>Student page</h1>')
 
+
+class StudentDetailView(DetailView):
+    template_name = 'teacher/student.html'
+    model = Student
+
+    def get(self, *args, **kwargs):
+        if 'mail' not in self.request.session:
+            return HttpResponseRedirect('../authorization/')
+        if 'role' != 'student':
+            return HttpResponseRedirect('../authorization/')
+        return super(StudentDetailView, self).get(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
